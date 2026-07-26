a2dc896a5b328a02ebc5eda2dbfc7607f74c3573
Daniel Joelsons <danieljoelsons@Mac.lan>
2026-07-25 19:39:05 -0700

    Add local preferences history and theme

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'medidas-v4';
+const CACHE = 'medidas-v5';
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['./', './index.html', './style.css', './app.js', './manifest.webmanifest', './icon.png', './apple-touch-icon.png'])).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(response => response || fetch(event.request))));
